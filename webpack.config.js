a4dd981ff391f070542675ca27b7d2ce7288b9e0
ivan-caro <ivan.caro.leiva@gmail.com>
2023-05-22 22:15:58 -0400

    Agregando primera configuracion de webpack.

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const htmlwebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new htmlwebpackplugin()
+    ]
+}
